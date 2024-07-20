9ff1e877e0e2890b381033b1d5b12c6280dd76da
Tomoaki-Moriya <tomoaki.moriya1096@gmail.com>
2024-07-20 19:38:00 +0900

    add cognito authorizer

--- project/aws-multitenant-s3-sample-api/app.py
+++ project/aws-multitenant-s3-sample-api/app.py
@@ -1,29 +1,15 @@
-from chalice import Chalice
+import os
+from chalice import Chalice, CognitoUserPoolAuthorizer
+
+COGNITO_USER_POOL_ARN = os.environ["COGNITO_USER_POOL_ARN"]
 
 app = Chalice(app_name="aws-multitenant-s3-sample-api")
+authorizer = CognitoUserPoolAuthorizer(
+    "AwsMultitenantS3SampleAuthorizer",
+    provider_arns=[COGNITO_USER_POOL_ARN],
+)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"], authorizer=authorizer)
 def index():
     return {"hello": "world"}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
