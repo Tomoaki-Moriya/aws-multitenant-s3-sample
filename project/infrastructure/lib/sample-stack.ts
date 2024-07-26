b8c2f01e4bc93ae93ae07e657f17aa04f7de09ac
Tomoaki-Moriya <tomoaki.moriya1096@gmail.com>
2024-07-26 12:55:13 +0900

    add

--- project/infrastructure/lib/sample-stack.ts
+++ project/infrastructure/lib/sample-stack.ts
@@ -10,51 +10,16 @@ export class SampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const userPool = new cognito.UserPool(this, `${PREFIX}-userpool-id`, {
-      userPoolName: `${PREFIX}-userpool`,
-      signInAliases: { email: true },
-      standardAttributes: {
-        email: {
-          required: true,
-          mutable: false,
-        },
-      },
-      customAttributes: {
-        tenant_id: new cognito.StringAttribute(),
-      },
-    });
+    const { userPool, identityPool } = this.createCognitoAuthentification();
+    const bucket = this.createBucket();
+    this.attachRole(userPool, bucket, identityPool);
+  }
 
-    const identityPool = new cognito.CfnIdentityPool(
-      this,
-      `${PREFIX}-identity-pool-id`,
-      {
-        allowUnauthenticatedIdentities: false,
-        cognitoIdentityProviders: [
-          {
-            clientId: userPool.userPoolId,
-            providerName: userPool.userPoolProviderName,
-          },
-        ],
-      }
-    );
-
-    new cognito.CfnIdentityPoolPrincipalTag(
-      this,
-      `${PREFIX}-identity-pool-tag-id`,
-      {
-        identityPoolId: identityPool.ref,
-        identityProviderName: userPool.userPoolProviderName,
-        principalTags: {
-          tenant_id: "custom:tenant_id",
-        },
-      }
-    );
-
-    const bucket = new s3.Bucket(this, `${PREFIX}-bucket`, {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      bucketName: `${PREFIX}-bucket`,
-    });
-
+  private attachRole(
+    userPool: cdk.aws_cognito.UserPool,
+    bucket: cdk.aws_s3.Bucket,
+    identityPool: cdk.aws_cognito.CfnIdentityPool
+  ) {
     const role = new iam.Role(this, `${PREFIX}-role`, {
       assumedBy: new iam.FederatedPrincipal(
         "cognito-identity.amazonaws.com",
@@ -87,5 +52,71 @@ export class SampleStack extends cdk.Stack {
         },
       }
     );
+  }
+
+  private createCognitoAuthentification(): {
+    userPool: cognito.UserPool;
+    identityPool: cognito.CfnIdentityPool;
+  } {
+    const userPool = new cognito.UserPool(this, `${PREFIX}-userpool-id`, {
+      userPoolName: `${PREFIX}-userpool`,
+      signInAliases: { email: true },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: false,
+        },
+      },
+      customAttributes: {
+        tenant_id: new cognito.StringAttribute(),
+      },
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(
+      this,
+      `${PREFIX}-userpool-client-id`,
+      {
+        userPool,
+        userPoolClientName: `${PREFIX}-userpool-client`,
+        authFlows: {
+          userPassword: true,
+          userSrp: true,
+        },
+      }
+    );
+
+    const identityPool = new cognito.CfnIdentityPool(
+      this,
+      `${PREFIX}-identity-pool-id`,
+      {
+        allowUnauthenticatedIdentities: false,
+        cognitoIdentityProviders: [
+          {
+            clientId: userPoolClient.userPoolClientId,
+            providerName: userPool.userPoolProviderName,
+          },
+        ],
+      }
+    );
+
+    new cognito.CfnIdentityPoolPrincipalTag(
+      this,
+      `${PREFIX}-identity-pool-tag-id`,
+      {
+        identityPoolId: identityPool.ref,
+        identityProviderName: userPool.userPoolProviderName,
+        principalTags: {
+          tenant_id: "custom:tenant_id",
+        },
+      }
+    );
+    return { userPool, identityPool };
+  }
+
+  private createBucket(): s3.Bucket {
+    return new s3.Bucket(this, `${PREFIX}-bucket`, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: `${PREFIX}-bucket`,
+    });
   }
 }
